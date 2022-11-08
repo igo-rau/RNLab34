bf6c2bbda0f1bd5cecc37c979026866100295937
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-11-08 19:17:10 +0200

    state management for textinput added

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Button } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='Your course goal!' />
-        <Button title='Add Goal'/>
-
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
 
       <Text style={styles.dummyText}>Igor Rautiainen Lab34!</Text>
@@ -21,32 +35,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
-    flexDirection:'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-
+    borderBottomColor: "#ccc",
   },
   TextInput: {
     borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
+    borderColor: "#ccc",
+    width: "70%",
     marginRight: 8,
-    padding: 8
-  }, 
+    padding: 8,
+  },
   goalsContainer: {
-    flex: 5
-  }
-
-
+    flex: 5,
+  },
 
   // container: {
   //   flex: 1,
