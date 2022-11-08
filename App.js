07afb69d15ad4ddd79c36f85f7119ca5f42385ca
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-11-08 18:40:03 +0200

    First components added

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Igor Rautiainen Lab34!</Text>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal'/>
+
       </View>
-      <Text
-        style={styles.dummyText}
-      >
-        Igor Rautiainen Lab34!
-      </Text>
-      <Button title="Fine!" />
+
+      <Text style={styles.dummyText}>Igor Rautiainen Lab34!</Text>
+
+      <View>
+        <Text>List of goals to come...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "#F88",
-    padding: 16,
-  },
+  appContainer: {
+    padding: 50
+  }
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // dummyText: {
+  //   margin: 16,
+  //   borderWidth: 2,
+  //   borderColor: "#F88",
+  //   padding: 16,
+  // },
 });
