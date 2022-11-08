d2f58773b3265e64434d3de7f9f90add9f6f3839
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-11-08 21:03:03 +0200

    GoalInput extracted as a separate component

--- App.js
+++ App.js
@@ -2,39 +2,27 @@ import { useState } from "react";
 import {
   StyleSheet,
   Text,
-  TextInput,
   View,
-  Button,
   FlatList,
 } from "react-native";
 
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      {text: enteredGoalText, id: Math.random().toString()}, // not actually a unique key!
+      { text: enteredGoalText, id: Math.random().toString() }, // not actually a unique key!
     ]);
   }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
+
 
       <Text style={styles.dummyText}>Igor Rautiainen Lab34!</Text>
 
@@ -42,11 +30,11 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <GoalItem text={itemData.item.text}/>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
-          keyExtractor={(item, index)=>{ return item.id}}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
           alwaysBounceVertical={false}
         />
       </View>
@@ -60,24 +48,8 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  TextInput: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 5,
   },
-
 });
